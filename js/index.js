1e369e6b23ef63f1f93a3433572703b09d20e9fa
Ruan Schoeman  231352 <125902884+Ruanschx277@users.noreply.github.com>
2023-05-12 09:21:04 +0200

    Ruan schoeman
    231352

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.log("Hello")\
+
+let Ronnie= 10;
+let Sam=23;
+let Timmy=10;
+
+Timmy+=10;
+
+let teamTotal=Timmy+Sam;
+let victory = Ronnie/2;
+let total = teamTotal + victory;
+
+
+let answer ="Timmy & Sam now have" + total + "Marbles!";
+
+console.log(answer)
+
+
